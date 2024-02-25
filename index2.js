e6736b531a2e410b9630a617c3db9274accf3594
LZD15 <s13994344109@126.com>
2024-02-25 15:16:42 +0800

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,33 @@
+(()=>{
+    const frontend = "https://limestart.cn/?from=ext";
+    const isAndroid = navigator.userAgent.includes("Android");
+    const isEdge = navigator.userAgent.includes("Edg/");
+    const isFirefox = navigator.userAgent.includes("Firefox/");
+    if (isAndroid) {
+        location.href = frontend;
+        return
+    }
+    chrome.storage.sync.get(["redirectNewTab"], result=>{
+        if (result.redirectNewTab) {
+            if (isFirefox) {
+                chrome.tabs.getCurrent(tab=>{
+                    chrome.tabs.create({
+                        url: frontend
+                    }, ()=>{
+                        chrome.tabs.remove(tab.id)
+                    }
+                    )
+                }
+                )
+            } else {
+                location.href = frontend
+            }
+        }
+    }
+    );
+    const iconLink = document.createElement("link");
+    iconLink.rel = "icon";
+    iconLink.href = isEdge ? "images/favicon-newtabpage.png" : "images/Lime_Icon_256x.png";
+    document.head.appendChild(iconLink)
+}
+)();
